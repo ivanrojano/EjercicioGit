9e62ff66c0072781e749c586c03be2b939bc6816
ivanrojano <165648051+ivanrojano@users.noreply.github.com>
2024-04-12 10:10:32 +0200

    Update Hora.java

--- ProyectoGit/src/Hora.java
+++ ProyectoGit/src/Hora.java
@@ -1,4 +1,45 @@
-
 public class Hora {
+	
+	//Propiedades de instancia
+	private int hora; 
+	private int minuto;
+	
+	/**
+	 * Constructor de la clase Hora
+	 * @param hora
+	 * @param minuto
+	 */
+	public Hora(int hora, int minuto) {
+		super();
+		this.hora = hora;
+		this.minuto = minuto;
+	}
 
+	//Getter de hora
+	public int getHora() {
+		return hora;
+	}
+
+	//Setter de hora
+	public void setHora(int hora) {
+		this.hora = hora;
+	}
+
+	//Getter de minuto
+	public int getMinuto() {
+		return minuto;
+	}
+
+	//Setter de minuto
+	public void setMinuto(int minuto) {
+		this.minuto = minuto;
+	}
+
+	@Override
+	//toString
+	public String toString() {
+		return "Hora [hora=" + hora + ", minuto=" + minuto + "]";
+	}
+	
+	
 }
